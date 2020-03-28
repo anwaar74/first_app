9562192aab7ec3aff85597b2aa05502d64877aa2
Anwaar Abdullah <Anwaar.Abdullah@datacom.com.au>
2020-03-28 11:03:14 +0800

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="first_app.MvcApplication" Language="C#" %>
---
+++ Scripts/main.js
@@ -0,0 +1,15 @@
+﻿
+now = new Date();
+localtime = now.toString();
+utctime = now.toGMTString();
+hour = now.getHours();
+min = now.getMinutes();
+sec = now.getSeconds();
+
+document.write("<b>Local time:</b> " + localtime + "<BR>");
+document.write("<b>UTC time:</b> " + utctime);
+var myarray = ['a', 'b', 'c', 'd', 123, 5678];
+alert(localtime);
+console.log(localtime);
+console.log(myarray[1], myarray[4]);
+console.log(hour + ':' + min + ':' + sec);
